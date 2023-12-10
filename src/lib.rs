bf4bd4e4f25215c7d27e690b0357da4a3d2fbdc7
tifoss <me@tifoss.eu>
2023-12-10 21:37:36 +0100

    first integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use actix_web::{web, App, HttpServer, HttpResponse};
+use actix_web::dev::Server;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().into()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+    
+    Ok(server)
 }
 
 #[cfg(test)]
