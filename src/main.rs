59d61837534a1414c39b45ce84fa7aa60499c269
tifoss <me@tifoss.eu>
2023-12-11 19:28:18 +0100

    random port for tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    run(TcpListener::bind("127.0.0.1:8000")?)?.await
 }
 
